2e767fcc87c16388fc06402cb7b302b86df1d0e2
Arte Ebrahimi <artee@uci.edu>
2015-07-02 18:38:46 -0700

    first commit

---
+++ snake.py
@@ -0,0 +1,133 @@
+import pygame
+import random
+import time
+
+#necessary pygame initializing
+pygame.init()
+
+#define screen size
+SCREEN_SIZE = 600
+
+#create a surface that will be seen by the user
+screen =  pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+
+SNAKE_SIZE = 15;
+
+direction = [[0, SNAKE_SIZE], [SNAKE_SIZE, 0], [0, -SNAKE_SIZE], [-SNAKE_SIZE, 0]]
+
+head_direction = 1
+
+clock = pygame.time.Clock()
+
+#snake initialization
+snake = [[260, 300], [285, 300], [300, 300]]
+
+#random seed
+random.seed()
+
+done = False
+lose = False
+
+#score
+score = 0;
+
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+PURP = (135, 0, 255)
+
+#summon initial apple
+apple_pos = [int(random.randint(0, 600 - SNAKE_SIZE)/SNAKE_SIZE)*SNAKE_SIZE, int(random.randint(0, 600 - SNAKE_SIZE)/SNAKE_SIZE)*SNAKE_SIZE]
+
+#summon borders
+pygame.mixer.music.load('bb-game.mid')
+pygame.mixer.music.play(-1, 0)
+
+
+while not done:
+	#white background
+	screen.fill(WHITE)
+
+	#head def
+	head = snake[len(snake) - 1]
+
+	#draw snake
+	for x in snake:
+		pygame.draw.rect(screen, BLACK, (x[0], x[1], SNAKE_SIZE, SNAKE_SIZE))
+
+	#score text
+	font = pygame.font.Font(None, 36)
+	score_txt = font.render("Score: " + str(score), 1, (10, 10, 10))
+	screen.blit(score_txt, (260, 36))
+
+	#draw apple
+	pygame.draw.rect(screen, BLACK, (apple_pos[0], apple_pos[1], SNAKE_SIZE, SNAKE_SIZE))
+
+	for event in pygame.event.get():  # User did something
+		if event.type == pygame.QUIT:  # If user clicked close
+			done = True  # Flag that we are done so we exit this loop	
+		if event.type == pygame.KEYDOWN:
+			if (event.key == pygame.K_UP and not head_direction == 0):
+				head_direction = 2
+				#head[0] = int((head[0] / SNAKE_SIZE)) * SNAKE_SIZE
+			elif (event.key == pygame.K_RIGHT and not head_direction == 3):
+				head_direction = 1
+				#head[1] = int((head[1] / SNAKE_SIZE)) * SNAKE_SIZE
+			elif (event.key == pygame.K_DOWN and not head_direction == 2):
+				head_direction = 0
+				#head[0] = int((head[0] / SNAKE_SIZE)) * SNAKE_SIZE
+			elif (event.key == pygame.K_LEFT and not head_direction == 1):
+				head_direction = 3
+				#head[1] = int((head[1] / SNAKE_SIZE)) * SNAKE_SIZE
+
+
+	#check for eating apples
+	if (head[0] == apple_pos[0] and head[1] == apple_pos[1]):
+		snake.append([head[0] + direction[head_direction][0], head[1] + direction[head_direction][1]])
+		head = snake[len(snake) - 1]
+		score += 1
+		#new apple position
+		apple_pos = [int(random.randint(SNAKE_SIZE, 600 - 2 * SNAKE_SIZE)/SNAKE_SIZE)*SNAKE_SIZE, int(random.randint(SNAKE_SIZE, 600 - 2 * SNAKE_SIZE)/SNAKE_SIZE)*SNAKE_SIZE]
+
+	#check for losing
+	for l in range(0, len(snake) - 2):
+		if (head[0] == snake[l][0] and head[1] == snake[l][1]):
+			lose = True
+			score = 0
+
+	#check for hit border
+	if (head[0] == 0 or head[0] == (SCREEN_SIZE - SNAKE_SIZE) or head[1] == 0 or head[1] == (SCREEN_SIZE - SNAKE_SIZE)):
+		lose = True;
+		score = 0
+
+	if lose == True:
+		#while (len(snake) > 3):
+			#snake.pop()
+		lose = False;
+		snake = [[260, 300], [285, 300], [300, 300]]
+		head = snake[len(snake) - 1]
+		head_direction = 1
+
+
+	#draw borders
+	pygame.draw.line(screen, PURP, (0, 0), (600, 0), 30)
+	pygame.draw.line(screen, PURP, (0, 0), (0, 600), 30)
+	pygame.draw.line(screen, PURP, (0, 600), (600, 600), 32)
+	pygame.draw.line(screen, PURP, (600, 600), (600, 0), 32)
+
+	##move snake##
+	snake.pop(0)
+	#snake.append([head[0] + direction[head_direction][0], head[1] + direction[head_direction][1]])
+	snake.append([int((head[0] / SNAKE_SIZE)) * SNAKE_SIZE + direction[head_direction][0], int((head[1] / SNAKE_SIZE)) * SNAKE_SIZE + direction[head_direction][1]])
+
+	print ("Head position: [" + str(head[0]) + ", " + str(head[1]) + "]")
+
+	pygame.display.flip()
+
+	clock.tick(15)
+
+
+pygame.quit()
+
+
+
+
